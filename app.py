607c4f9ed0bb6ed07c7640d7a6bd6af53a977500
Nimo139 <git@nimohr.de>
2022-01-02 02:19:16 +0100

    updating status

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for  # For flask implementation
+from flask import Flask, render_template, request, redirect, url_for, Response  # For flask implementation
 from soco import SoCo
 
 app = Flask(__name__)
@@ -10,13 +10,75 @@ def main():
     track = sonos.get_current_track_info()
     volume = sonos.volume
 
-    return render_template('index.html', track=track, volume=volume)
+    return render_template('index.html', track=track, volume=volume, playing=is_playing())
 
 
 @app.route("/track")
 def get_current_track():
     track = sonos.get_current_track_info()
     return track
+
+
+@app.route("/state")
+def get_state():
+
+    state = sonos.get_current_track_info()
+    # h:mm:ss to seconds
+    state["position"] = time_to_seconds(state["position"])
+    state["duration"] = time_to_seconds(state["duration"])
+
+    state["is_playing"] = is_playing()
+    state["volume"] = sonos.volume
+    state.pop("metadata")
+
+    return state
+
+
+def time_to_seconds(time):
+    time = time.split(":")
+    return int(time[0]) * 3600 + int(time[1]) * 60 + int(time[2])
+
+
+@app.route("/play")
+def play():
+    sonos.play()
+    status_code = Response(status=200)
+    return status_code
+
+
+@app.route("/pause")
+def pause():
+    sonos.pause()
+    status_code = Response(status=200)
+    return status_code
+
+
+@app.route("/volume")
+def volume():
+    return str(sonos.volume)
+
+
+@app.route("/volumeup")
+def volumeup():
+    sonos.volume += 1
+    status_code = Response(status=200)
+    return status_code
+
+
+@app.route("/volumedown")
+def volumedown():
+    sonos.volume -= 1
+    status_code = Response(status=200)
+    return status_code
+
+
+def is_playing():
+    """
+    Get the playing state (Playing/transitioning or Paused/stopped)
+    :return: boolean
+    """
+    state = sonos.get_current_transport_info()['current_transport_state']
+    return state in ['PLAYING', 'TRANSITIONING']
 
 
 if __name__ == "__main__":
